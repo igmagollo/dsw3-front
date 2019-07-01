4140ad75b771d2cc0e57151caa8146fd212abf3c
Igor Magollo <igraphmagollo@gmail.com>
2019-07-01 10:23:26 -0300

    Vamo que vamo

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import {UserService} from '../../shared/services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,8 @@ export class LoginComponent implements OnInit {
   isSubmitting = false;
   hasError = false;
 
-  constructor(private fb: FormBuilder, private user: UserService) { }
+  constructor(private fb: FormBuilder, private user: UserService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -30,6 +32,7 @@ export class LoginComponent implements OnInit {
     const values = this.loginGroup.getRawValue();
     this.user.login(values.username, values.password).subscribe(res => {
       this.user.initialize(res);
+      this.router.navigate(['/']);
     }, err => {
       this.hasError = true;
       this.isSubmitting = false;
